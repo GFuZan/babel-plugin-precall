9cd5d9372eb1740fa33f14a8fcf441658992d5e3
gfuzan <gfuzan@163.com>
2024-08-07 09:45:47 +0800

    feat: 支持 import ... from 'precall(`...`)' 语法

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const plugin = function (babel, options = {}) {
         rootPath.traverse(traverseOptions, {
           filename: state.filename,
           ...ops,
+          isBabel6: babel.version.startsWith("6."),
           babel,
         });
       },
@@ -44,6 +45,55 @@ const plugin = function (babel, options = {}) {
  * @type {import('@babel/core').Visitor}
  */
 const traverseOptions = {
+  ImportDeclaration(path, state) {
+    /**
+     * @type {{ babel: import('@babel/core') }}
+     */
+    const {
+      filename = "p.js",
+      keyRegExp,
+      isBabel6,
+      babel,
+      babel: { types: t },
+    } = state;
+
+    if (
+      t.isStringLiteral(path.node.source) &&
+      keyRegExp.test(path.node.source.value)
+    ) {
+      path.node.source = getNodeValue(
+        babel,
+        path.node.source.value,
+        filename,
+        isBabel6
+      );
+    }
+  },
+  CallExpression(path, state) {
+    /**
+     * @type {{ babel: import('@babel/core') }}
+     */
+    const {
+      filename = "p.js",
+      keyRegExp,
+      isBabel6,
+      babel,
+      babel: { types: t },
+    } = state;
+
+    if (
+      path.node.callee.name === "require" &&
+      t.isStringLiteral(path.node.arguments[0]) &&
+      keyRegExp.test(path.node.arguments[0].value)
+    ) {
+      path.node.arguments[0] = getNodeValue(
+        babel,
+        path.node.arguments[0].value,
+        filename,
+        isBabel6
+      );
+    }
+  },
   Identifier(path, state) {
     /**
      * @type {{ babel: import('@babel/core') }}
@@ -119,6 +169,24 @@ const replaceNode = (path, babel, moduleValue, filename) => {
 };
 
 /**
+ *
+ * @param {import('@babel/core')} babel
+ * @param {string} value
+ * @param {string} filename
+ * @returns {import('@babel/core').Node}
+ */
+const getNodeValue = (babel, value, filename, isBabel6) => {
+  if (isBabel6) {
+    const { transform } = babel;
+    return transform(`m.e=${value}`).ast.program.body[0].expression.right;
+  }
+
+  const { parse, parseSync = parse } = babel;
+
+  return parseSync(value, { filename }).program.body[0].expression;
+};
+
+/**
  * 处理入参
  * @param {{ value: string | Array<string> }} options
  * @returns
@@ -131,8 +199,11 @@ const handleOptions = (options) => {
     return sum;
   }, {});
 
+  const keyRegExp = new RegExp(`^(${keyList.join("|")})[(\`]`);
+
   return {
     keyList,
+    keyRegExp,
     keyMap,
   };
 };
